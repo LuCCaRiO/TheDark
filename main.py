8576696de056a28173a5af28545d1d0bfa1811ab
LuCCaRiO <janlustockmann@gmail.com>
2023-11-14 21:06:27 +0100

    Setup

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame as pg
+from settings import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+class Game:
+    def __init__(self):
+        self.screen = pg.display.set_mode((1920, 1080), pg.FULLSCREEN)
+
+    def run(self):
+        clock = pg.time.Clock()
+        while True:
+            self.handle_events()
+            clock.tick(FPS)
+
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    Game().run()
