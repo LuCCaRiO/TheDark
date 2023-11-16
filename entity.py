b374642f24da5e2022d7f7fbb940dcb6b1b13ad3
LuCCaRiO <janlustockmann@gmail.com>
2023-11-16 17:02:05 +0100

    Added player jump capability

--- entity.py
+++ entity.py
@@ -25,6 +25,7 @@ class MoveableEntity(Entity):
 class Player(MoveableEntity):
     SPEED = 7
     GRAVITY = 0.5
+    JUMP_FORCE = -4
 
     def __init__(self, pos, groups, collidable_sprites):
         sprite_sheet = [pg.image.load("images/BlackSprite/blackSprite_0.png"),
@@ -39,22 +40,27 @@ class Player(MoveableEntity):
         self.direction = pg.math.Vector2()
         self.hitbox = self.rect.inflate(-10, 0)
 
+        self.on_ground = False
+
         self.collidable_sprites = collidable_sprites
 
     def animation(self):
-        self.anm_index += self.delta_time / 130
+        self.anm_index = self.delta_time / 130
         if self.anm_index >= len(self.sprite_sheet) or self.direction == pg.math.Vector2():
             self.anm_index = 0
         self.scale_up(self.sprite_sheet, int(self.anm_index), TILE_SIZE / 8)
 
     def input(self):
         keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT]:
+        if keys[pg.K_a]:
             self.direction.x = -1
-        elif keys[pg.K_RIGHT]:
+        elif keys[pg.K_d]:
             self.direction.x = 1
         else:
             self.direction.x = 0
+
+        if keys[pg.K_w] and self.on_ground:
+            self.direction.y = Player.JUMP_FORCE
 
     def gravity(self):
         self.direction.y += Player.GRAVITY * (self.delta_time / RELATION_DELTA_TIME)
@@ -70,6 +76,7 @@ class Player(MoveableEntity):
         self.hitbox.centery = round(self.pos.y)
         self.rect.centery = self.hitbox.centery
 
+        self.on_ground = False
         self.collision(VERTICAL)
 
     def collision(self, direction):
@@ -82,9 +89,11 @@ class Player(MoveableEntity):
                         self.hitbox.left = sprite.rect.right
                     self.rect.centerx = self.hitbox.centerx
                     self.pos.x = self.hitbox.centerx
+
                 elif direction == VERTICAL:
                     if self.direction.y > 0:
                         self.hitbox.bottom = sprite.rect.top
+                        self.on_ground = True
                     elif self.direction.y < 0:
                         self.hitbox.top = sprite.rect.bottom
                     self.direction.y = 0
